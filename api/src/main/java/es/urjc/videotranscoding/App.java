3add6d095cda361b346b3e82a0ead462977db55a
Luis Carlos <luisca_jl@hotmail.com>
2020-06-24 16:26:51 +0200

    Update to postgres

--- api/src/main/java/es/urjc/videotranscoding/App.java
+++ api/src/main/java/es/urjc/videotranscoding/App.java
@@ -1,19 +1,10 @@
 package es.urjc.videotranscoding;
 
-import org.apache.coyote.UpgradeProtocol;
-import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Main APP and import xml config
- * 
- * @author luisca
- */
 @SpringBootApplication
 @Slf4j
 public class App {
@@ -23,18 +14,5 @@ public class App {
 		log.info(" --------- App SpringBoot Started ------- ");
 	}
 
-	// @Bean
-	// public TomcatConnectorCustomizer http2ProtocolCustomizer() {
-	// 	return (connector) -> {
-	// 		for (UpgradeProtocol upgradeProtocol : connector.findUpgradeProtocols()) {
-	// 			if (upgradeProtocol instanceof Http2Protocol) {
-	// 				Http2Protocol http2Protocol = (Http2Protocol) upgradeProtocol;
-	// 				http2Protocol.setOverheadContinuationThreshold(0);
-	// 				http2Protocol.setOverheadDataThreshold(0);
-	// 				http2Protocol.setOverheadWindowUpdateThreshold(0);
-	// 			}
-	// 		}
-	// 	};
-	// }
 
 }
